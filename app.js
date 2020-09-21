2c62fc20c9b0d7ea4bf910eda6c07e0b44a798a9
stevenharja <stevenharja@gmail.com>
2020-09-21 21:17:20 +1000

    implemented CORS

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
+const cors = require('cors');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -24,6 +25,14 @@ app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 
 // 1) GLOBAL MIDDLEWARES
+//Implement CORS, this applies CORS everywhere, sets Access-Control-Allow-Origin * to everywhere
+// app.use(cors({ <- this is for signle site CORS enabling
+//  origin: 'https://www.natours.com'
+//}))
+app.use(cors());
+
+app.options('*', cors());
+// app.options('/api/v1/tours/:id', cors()); <- For only one url
 // Serving static files
 app.use(express.static(path.join(__dirname, 'public')));
 
